5775c2591093ba49d65474d70a6bbfff2d331c11
Benedikt Jazdzewski <jazdzewski@b1-systems.de>
2021-12-09 11:18:21 +0100

    removed webvitals, so no 1chunk.js is build with "make horde"

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 window.onload = () => {
   if (!globalThis.horde) {
@@ -25,8 +24,3 @@ window.onload = () => {
     document.getElementById('root'),
   );
 };
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
